f09e790f0a2b79e76245cdb12f6e90fcdf58a3d7
Evgeniy Malygin <black2001@inbox.ru>
2022-09-22 15:18:28 +0300

    Выполнено задание №4

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>(List.of(8, 1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
 // Задание №1
         System.out.println("======================= Задание №1 =======================");
-        for (Integer num : nums) {
+        for (int num : nums) {
             if (num % 2 != 0) {
                 System.out.print(num + " ");
             }
@@ -29,12 +29,13 @@ public class Main {
 
         // Задание №3
         System.out.println("======================= Задание №3 =======================");
-        List <String> words = new ArrayList<>(List.of("окно", "мыло", "село", "капуста", "окно", "село"));
+        List<String> words = new ArrayList<>(List.of("окно", "мыло", "село", "капуста", "окно", "село"));
         Set<String> uniqueWord = new HashSet<>(words);
         System.out.println(uniqueWord);
 
-
+// Задание №4
+        System.out.println("======================= Задание №4 =======================");
+        System.out.println(words.size() - uniqueWord.size());
     }
-
 
 }
