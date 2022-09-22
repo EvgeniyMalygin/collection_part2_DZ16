b306698f5943caa680ef4a9e877770d7c9c65d2f
Evgeniy Malygin <black2001@inbox.ru>
2022-09-22 13:16:23 +0300

    Выполнено задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = new ArrayList<>(List.of(8, 1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+// Задание №1
+        System.out.println("======================= Задание №1 =======================");
+        for (Integer num : nums) {
+            if (num % 2 != 0) {
+                System.out.print(num + " ");
+            }
+        }
+        System.out.println();
     }
+
 }
